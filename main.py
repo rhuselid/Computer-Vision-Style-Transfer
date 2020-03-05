604b3233bb8cd949c2e725ddd4f3efc510bd083b
Robert Huselid <rhuselid@cs.uchicago.edu>
2020-03-05 11:14:45 -0600

    added partial optimization

--- main.py
+++ main.py
@@ -13,9 +13,15 @@ import numpy as np
 import os
 import math
 
+from PIL import Image
+
+
 
 vgg19 = models.vgg19(pretrained=True)
 # pretrained object detection deep CNN to use for feature extraction
+
+device = torch.device('cpu')
+
 
 def total_loss(content_loss, style_loss, alpha=0.5, beta=0.5):
 	return content_loss*alpha + style_loss*beta
@@ -26,8 +32,12 @@ def style_loss(style_img: torch.Tensor, noise: torch.Tensor):
 	returns the style loss between a style image layer and its corresponding
 	noise layer
 	"""
+	print(noise_gram.shape)
+	print(style_gram.shape)
+	
 	style_gram = torch.mm(style_img, style_image.t())
 	noise_gram = torch.mm(noise, noise.t())
+
 	return torch.sum(torch.pow(noise_gram - style_gram, 2)).div(4*(np.power(noise.shape[0]*noise.shape[1], 2)))
 
 def content_loss(content_img: torch.Tensor, noise: torch.Tensor):
@@ -38,11 +48,12 @@ def content_loss(content_img: torch.Tensor, noise: torch.Tensor):
 	return torch.sum(torch.pow(content_img - noise, 2)).div(2)
 
 
-def optimization():
+def create_optimizer(params, lr):
 	'''
 	optimizing between content loss (content image) and style loss (style image)
 	'''
-	pass
+	return torch.optim.Adam(params=params, lr=lr)
+
 
 def gather_features(x, style=False):
 	'''
@@ -104,8 +115,40 @@ def gather_features(x, style=False):
 		return gram_style_features
 
 
+def generate_output(content_image, style_image):
+	content_rows, content_cols, content_channels = content_image.shape
+	style_rows, style_cols, style_channels = style_image.shape
 
+	noise = torch.randn(content_rows, content_cols, content_channels, device=device, requires_grad=True)
 
-gather_features(torch.rand((64, 3, 32, 32)), style=True)
+	epochs = 2
+	start_lr = 0.01
+
+	for e in range(epochs):
+		optimizer = create_optimizer([noise], start_lr*(0.9**e))
+
+		optimizer.zero_grad()
+
+		current_content_loss = content_loss(content_image, noise)
+		current_style_loss = style_loss(style_image, noise)
+		current_total_loss = total_loss(current_content_loss, current_style_loss)
+
+		current_total_loss.backward()
+		optimizer.step()
+
+	return noise
+	
+
+if __name__ == '__main__':
+
+	content_path = './data/content/Cat.jpeg'
+	style_path = './data/style/VanGogh01.jpg'
+
+	content_image = torch.from_numpy(np.array(Image.open(content_path)))
+	style_image =  torch.from_numpy(np.array(Image.open(style_path)))
+
+	generate_output(content_image, style_image)
+
+# gather_features(torch.rand((64, 3, 32, 32)), style=True)
 
 
