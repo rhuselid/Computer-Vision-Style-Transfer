fb67988afe214dc09516ffe83b5f04324514b4a4
Robert Huselid <rhuselid@cs.uchicago.edu>
2020-03-05 19:14:54 -0600

    changed grad to noise and not to the origional image

--- main.py
+++ main.py
@@ -116,7 +116,9 @@ def gather_features(x, style=False, noise=False):
 			if not noise:
 				if i in content_layers:
 					intermediary_outputs.append(x.detach())
-				else:
+				
+			else:
+				if i in content_layers:
 					intermediary_outputs.append(x)
 
 	# note that intermediary outputs is a list of numpy arrays of different shapes (differ between layers)
@@ -134,8 +136,6 @@ def gather_features(x, style=False, noise=False):
 
 			# unroll the layer output into column form (vectorized multiplication)
 			current_shape = intermediary_layer.shape
-			# print(current_shape)
-			# .squeeze().view(intermediary_outputs[i].shape[1], -1)
 			reshaped_layer = intermediary_layer.squeeze().view(current_shape[1], -1)
 
 			gram_matrix = torch.mm(reshaped_layer, reshaped_layer.T)
@@ -149,59 +149,66 @@ def generate_output(content_image, style_image):
 	batch_num_c, content_rows, content_cols, content_channels = content_image.shape
 	batch_num_s, style_rows, style_cols, style_channels = style_image.shape
 
-	noise = torch.randn(1, content_rows, content_cols, content_channels, device=device, requires_grad=True)
+	noise = torch.empty(1, content_rows, content_cols, content_channels).uniform_(0, 1)
+	noise.requires_grad = True
+	# noise = torch.randn(1, content_rows, content_cols, content_channels, device=device, requires_grad=True)
 
 	content_activation = gather_features(content_image, style=False)
 	style_activations = gather_features(style_image, style=True)
 	
-	iterations = 250
-	start_lr = 0.1
+	iterations = 100
+	current_learn_rate = 0.1
 
 	for iter_num in range(iterations):
-		current_learn_rate = max(1e-5, start_lr*(0.97**iter_num))
+		#current_learn_rate = max(1e-1, start_lr*(0.95**iter_num))
+		if iter_num > 50:
+			current_learn_rate = max(current_learn_rate*0.95, 0.01)
 
 		optimizer = create_optimizer([noise], current_learn_rate)
-		noise_content_activation = gather_features(noise, style=False)
-		# noise_content_activation.requires_grad = True
 
-		noise_style_activations = gather_features(noise, style=True)
+		noise_content_activation = gather_features(noise, style=False, noise=True)
 
-		# for i in range(len(noise_style_activations)):
-		# 	noise_style_activations[i].requires_grad = True
+		noise_style_activations = gather_features(noise, style=True, noise=True)
 
 		optimizer.zero_grad()
 
 		current_content_loss = content_loss(content_activation, noise_content_activation)
 		current_style_loss = style_loss(style_activations, noise_style_activations)
-		current_total_loss = total_loss(current_content_loss, current_style_loss, alpha=0.001, beta=0.999)
+		current_total_loss = total_loss(current_content_loss, current_style_loss, alpha=0.5, beta=0.5)
 
 		current_total_loss.backward()
 		optimizer.step()
 
-		if iter_num % 25 == 0:
+		if iter_num % 10 == 0:
 			print('Iteration Number', iter_num)
 			print('Style Loss:   ', int(current_style_loss.item()))
 			print('Content Loss: ', int(current_content_loss.item()))
 			print()
 
+	print('Output Tensor')
+	print(noise)
 	return noise
 	
 
 if __name__ == '__main__':
 
 	content_path = './data/content/Cat.jpeg'
-	style_path = './data/style/VanGogh01.jpg'
+	style_path = './data/style/picasso01.jpg'
 
 	content_image = np.array(Image.open(content_path)) / 255
 	content_image = np.moveaxis(content_image, -1, 0)
 	content_image = torch.from_numpy(content_image).unsqueeze(0).float()
+	# print(content_image.requires_grad)
 
 	style_image = np.array(Image.open(style_path)) / 255
 	style_image = np.moveaxis(style_image, -1, 0)
 	style_image = torch.from_numpy(style_image).unsqueeze(0).float()
+	# print(style_image.requires_grad)
 
 	output = generate_output(content_image, style_image).detach().numpy()[0,:,:,:]
 	output = np.moveaxis(output, 0, -1)
+
+	# np.savetxt('last_output.txt', output, delimiter=',')
 
 	plt.imshow(output)
 	plt.show()
